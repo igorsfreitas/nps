3420c47fe33eef1714a643e2534a4638f2055fde
Igor Freitas <igorfreitas@MacBook-Pro-de-Igor.local>
2019-01-18 22:24:39 -0300

    Add Routing module and authguard for login and logout services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { UserResolver } from './user/user.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
+  { path: 'user', component: UserComponent,  resolve: { data: UserResolver}}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
