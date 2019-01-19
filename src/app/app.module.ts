9107212cde7dcdbaa9ad9eb717d5a78104704ac8
Igor Freitas <igorfreitas@MacBook-Pro-de-Igor.local>
2019-01-18 22:26:51 -0300

    Firebase and user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,49 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
+import { UserResolver } from './user/user.resolver';
+import { AuthGuard } from './core/auth.guard';
+import { AuthService } from './core/auth.service';
+import { UserService } from './core/user.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularFireDatabase } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnu3vGZ_0jnayTq-2flezNJB9tClljOes",
+  authDomain: "testenps-dd817.firebaseapp.com",
+  databaseURL: "https://testenps-dd817.firebaseio.com",
+  projectId: "testenps-dd817",
+  storageBucket: "testenps-dd817.appspot.com",
+  messagingSenderId: "44535758902"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatDialogModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthService, UserService, UserResolver, AuthGuard, AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
